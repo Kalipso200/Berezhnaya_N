9ab6d8f30d2d5f1e58fc8173aa0780fdce2ab9cb
Nadya <nadetsarenkova@gmail.com>
2025-10-27 15:41:24 +0300

    update

---
+++ src/main/java/ru/rksp/Berezhnaya/BerezhnayaApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Berezhnaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerezhnayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerezhnayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/Berezhnaya/BerezhnayaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.Berezhnaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerezhnayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
